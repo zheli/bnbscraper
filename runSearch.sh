36053d516d94c07f81b5f80c69b3a8d62d0126a9
Ubuntu <ubuntu@ip-172-31-6-87.eu-central-1.compute.internal>
2016-02-20 11:51:32 +0000

    minor fix

--- runSearch.sh
+++ runSearch.sh
@@ -12,5 +12,5 @@ LOGFILE="/home/ubuntu/$LOGFILE"
 OUTFILE="$QUERY.json"
 OUTFILE="/home/ubuntu/$OUTFILE"
 echo $OUTFILE
-# scrapy crawl airbnb -a query=$QUERY -a filters=$FILTERS  -o $OUTFILE --loglevel INFO 2>&1 | tee $LOGFILE
+scrapy crawl airbnb -a query=$QUERY -a filters=$FILTERS  -o $OUTFILE --loglevel INFO 2>&1 | tee $LOGFILE
 
