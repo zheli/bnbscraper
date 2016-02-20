2365c18cc8273c97702fdd8a3ab2d9d87752dd6f
Ubuntu <ubuntu@ip-172-31-6-87.eu-central-1.compute.internal>
2016-02-20 11:47:25 +0000

    search script takes query argument

--- runSearch.sh
+++ runSearch.sh
@@ -2,7 +2,7 @@
 
 cd /home/ubuntu/AirBnBScrapingProject/bnbscraper
 
-QUERY="Rome--Italy"
+QUERY=$1
 FILTERS="neighborhood,property_type_id"
 DATE="$(date +%F)"
 LOGSEP="_"
@@ -12,9 +12,5 @@ LOGFILE="/home/ubuntu/$LOGFILE"
 OUTFILE="$QUERY.json"
 OUTFILE="/home/ubuntu/$OUTFILE"
 echo $OUTFILE
-echo $LOGFILE
-touch $OUTFILE
-touch $LOGFILE
-ls
-scrapy crawl airbnb -a query=$QUERY -a filters=$FILTERS  -o $OUTFILE --loglevel INFO 2>&1 | tee $LOGFILE
+# scrapy crawl airbnb -a query=$QUERY -a filters=$FILTERS  -o $OUTFILE --loglevel INFO 2>&1 | tee $LOGFILE
 
