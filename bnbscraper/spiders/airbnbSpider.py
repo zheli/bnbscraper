19d6459a61d7fb78691501e987bc09dbfd27b51b
LUCA_VERGINER <luca@verginer.eu>
2016-02-21 03:48:44 +0100

    added additional json field and added test to check for missing filter in the search site

--- bnbscraper/spiders/airbnbSpider.py
+++ bnbscraper/spiders/airbnbSpider.py
@@ -1,9 +1,7 @@
 import scrapy
 from bnbscraper.bnbItem import BnbItem
 import json
-import urllib
 import itertools
-
 
 AIRBNB_URL = "https://www.airbnb.com/s/"
 
@@ -13,16 +11,17 @@ class AirbnbSpider(scrapy.Spider):
     pass command line arguments as follows
     e.g. scrapy crawl airbnb -a query=Reggio-Emilia--Italy filters=neighborhood,propertytype-o tests.json
     """
+
     def __init__(self, query='', filters='', *args, **kwargs):
         super(AirbnbSpider, self).__init__(*args, **kwargs)
         self.start_urls = [AIRBNB_URL + query]
-        self.bnb_filters = [filter.strip() for filter in list(filters.split(','))]
+        self.bnb_filters = [b_filter.strip() for b_filter in list(filters.split(','))]
 
     name = "airbnb"
     allowed_domains = ["airbnb.com"]
 
     def parse(self, response):
-        filter_dict =  self.extract_filter_property(response)
+        filter_dict = self.extract_filter_property(response)
         # test if there are any neighborhoods
         if not filter_dict:
             # if not pass the first page to the parse result page
@@ -30,22 +29,23 @@ class AirbnbSpider(scrapy.Spider):
         else:
             all_combinations = self.filter_dict_to_tuple(filter_dict)
             for filter_combination in self.filter_combinations_generator(all_combinations):
-                # query_string = urllib.urlencode(filter_combination)
-                query_string = '&'.join([k+'='+v for k, v in filter_combination])
-                request_url = self.start_urls[0]+'?'+query_string
-
+                query_string = '&'.join([k + '=' + v for k, v in filter_combination])
+                request_url = self.start_urls[0] + '?' + query_string
                 yield scrapy.Request(request_url, callback=self.parse_start_page)
 
-
     def parse_start_page(self, response):
-        # this function is called to parse the individual links on the result page after any filter in the previous steps
-
+        """
+        this function is called to parse the individual links on
+        the result page after any filter in the previous steps
+        :param response:
+        :return:
+        """
         try:
             last_page_number = int(response
-                               .xpath('//ul[@class="list-unstyled"]/li[last()-1]/a/@href')
-                               .extract()[0]
-                               .split('page=')[1]
-                               )
+                                   .xpath('//ul[@class="list-unstyled"]/li[last()-1]/a/@href')
+                                   .extract()[0]
+                                   .split('page=')[1]
+                                   )
         except:
             last_page_number = 1
 
@@ -53,10 +53,10 @@ class AirbnbSpider(scrapy.Spider):
         if '?' in response.url:
             page_separator = '&'
         else:
-            page_separator= '?'
+            page_separator = '?'
         page_urls = [response.url + page_separator + "page=" + str(pageNumber)
                      for pageNumber
-                     in range(1, last_page_number+1)
+                     in range(1, last_page_number + 1)
                      ]
         # the function loops over all paginated result pages
 
@@ -68,14 +68,13 @@ class AirbnbSpider(scrapy.Spider):
             yield scrapy.Request(page_url, callback=self.parse_listing_results_page)
             # send a request every time and set as callback the parseQueryPage
 
-
     def parse_listing_results_page(self, response):
         for href in response.xpath('//div[@class="listing"]/@data-url').extract():
             url = response.urljoin(href)
-            yield scrapy.Request(url, callback=self.parse_dir_contents)
+            yield scrapy.Request(url, callback=self.parse_listing_contents)
 
-
-    def parse_dir_contents(self, response):
+    @staticmethod
+    def parse_listing_contents(response):
         """
         This method extracts the actual data from the airbnb listing
         :param response: scrapy response object
@@ -132,7 +131,7 @@ class AirbnbSpider(scrapy.Spider):
 
         bathrooms = response.xpath('//strong[contains(@data-reactid,"Bathrooms")]/text()').extract()
         if len(bathrooms):
-            if "+" in bathrooms: # some listings have 8+ rooms as a number
+            if "+" in bathrooms:  # some listings have 8+ rooms as a number
                 item['bathrooms'] = 9
             else:
                 item['bathrooms'] = float(bathrooms[0])
@@ -141,27 +140,29 @@ class AirbnbSpider(scrapy.Spider):
         if len(bedrooms):
             item['bedrooms'] = float(bedrooms[0])
 
-
         beds = response.xpath('//strong[contains(@data-reactid,"Beds")]/text()').extract()
         if len(beds):
             item['beds'] = float(beds[0])
 
-        propertyType = response.xpath('//strong[contains(@data-reactid,"Property type")]/text()').extract()
-        if len(propertyType):
-            item['propertyType'] = propertyType[0]
+        property_type = response.xpath('//strong[contains(@data-reactid,"Property type")]/text()').extract()
+        if len(property_type):
+            item['propertyType'] = property_type[0]
 
-        extraPeople = response.xpath('//strong[contains(@data-reactid,"Extra people")]/text()').extract()
-        if len(extraPeople):
-            item['extraPeople'] = extraPeople[0]
+        extra_people = response.xpath('//strong[contains(@data-reactid,"Extra people")]/text()').extract()
+        if len(extra_people):
+            item['extraPeople'] = extra_people[0]
 
-        cleaningFee = response.xpath('//strong[contains(@data-reactid,"Cleaning")]/text()').extract()
-        if len(cleaningFee):
-            item['cleaningFee'] = cleaningFee[0]
+        cleaning_fee = response.xpath('//strong[contains(@data-reactid,"Cleaning")]/text()').extract()
+        if len(cleaning_fee):
+            item['cleaningFee'] = cleaning_fee[0]
 
         item['neighborhood'] = response.request.url
 
-        yield item
+        iso_state_json  = response.xpath('//div[@class="___iso-state___listingbundlejs"]/@data-state').xtract()
+        if len(iso_state_json):
+            item['iso_state_json'] =  iso_state_json
 
+        yield item
 
     def extract_filter_property(self, response):
         """
@@ -169,29 +170,30 @@ class AirbnbSpider(scrapy.Spider):
         :param response:
         :return: a dict with the filters as keys and the possible values as lists
         """
-        filterDict = dict()
+        filter_dict = dict()
 
         if not self.bnb_filters:
-            return filterDict
+            return filter_dict
         else:
-            for filter in self.bnb_filters:
-                xpath_query_string = '//input[@name="%s"]/@value' % filter
-                filterDict[filter] = response.xpath(xpath_query_string).extract()
-            return filterDict
+            for b_filter in self.bnb_filters:
+                xpath_query_string = '//input[@name="%s"]/@value' % b_filter
+                filter_values = response.xpath(xpath_query_string).extract()
+                if filter_values:  # check if the provided filter is even available
+                    continue
+                else:
+                    filter_dict[b_filter] = filter_values
+            return filter_dict
 
-
-    def filter_dict_to_tuple(self, filter_dict):
+    @staticmethod
+    def filter_dict_to_tuple(filter_dict):
         """
         returns a list of lists of all possible combinations of key and value to be used in the url creation
-        use urllib.urlencode([first_tuple, second_tuple)]
         :param filter_dict:
         :return:
         """
         return [[(key, filter_dict[key][i]) for i in range(len(filter_dict[key]))] for key in filter_dict.keys()]
 
-
-    def filter_combinations_generator(self, combinations):
+    @staticmethod
+    def filter_combinations_generator(combinations):
         for element in itertools.product(*combinations):
             yield element
-
-
